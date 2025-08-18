92ea8e3babab632097df78a2933a7f49417970a9
Chris-A-75 <chrisaccaoui@protonmail.com>
2025-08-18 11:35:56 +0300

    Top bar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
-
+import "./App.css";
+import logo from "./assets/AGROTICA-2024-2.png";
 
 function App() {
   return (
     <div>
-      hi
+      {" "}
+      {/* root */}
+      <div className="top-bar-1">
+        <img src={logo} alt="logo" height="100%"></img>
+        <div
+          title="top-bar-1-text"
+          style={{
+            marginLeft: "20%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <div style={{ fontSize: 14, color: "grey" }}>
+            Call anytime <br></br>
+            <span style={{ fontWeight: "bold", color: "black" }}>
+              +961 3 21 22 30
+            </span>
+          </div>
+          <div
+            style={{
+              width: "1px",
+              height: "80px",
+              backgroundColor: "#ccc",
+              margin: 60,
+            }}
+          ></div>
+          <div style={{ fontSize: 14, color: "grey" }}>
+            Send email <br></br>
+            <span style={{ fontWeight: "bold", color: "black" }}>
+              Info@Agrotica.info
+            </span>
+          </div>
+          <div
+            style={{
+              width: "1px",
+              height: "80px",
+              backgroundColor: "#ccc",
+              margin: 60,
+            }}
+          ></div>
+          <div style={{ fontSize: 14, color: "grey" }}>
+            Al Chouf - Mahafer - Main Street <br></br>
+            <span style={{ fontWeight: "bold", color: "black" }}>Al Chouf</span>
+          </div>
+          {/* add fb and insta pics later */}
+        </div>
+      </div>
+      <div className="top-bar-2"></div>
     </div>
   );
 }
