1850bb3aa1bf2ac6dd926ef3aa77956c9a34985a
Chris A <chrisaccaoui@pprotonmail.com>
2025-08-25 10:36:02 +0300

    section 1

--- src/App.js
+++ src/App.js
@@ -51,57 +51,35 @@ function App() {
 </div>
 
       <div className="top-bar-2">
-        <a className="bar-link-text" href="/home">
-          Home
-        </a>
-        <a className="bar-link-text" href="/about">
-          About us
-        </a>
-        <a className="bar-link-text" href="/products">
-          Products
-        </a>
-        <a className="bar-link-text" href="/projects">
-          Projects
-        </a>
-        <a className="bar-link-text" href="/contact">
-          Contact us
-        </a>
-      </div>
+  <a className="bar-link-text" href="/home">Home</a>
+  <a className="bar-link-text" href="/about">About us</a>
+  <a className="bar-link-text" href="/products">Products</a>
+  <a className="bar-link-text" href="/projects">Projects</a>
+  <a className="bar-link-text" href="/contact">Contact us</a>
+</div>
+
       <div
-        className="section-1"
-        style={{ position: "relative", display: "inline-block", width: "100%" }}
-      >
-        <img
-          src={image1}
-          alt=""
-          style={{ width: "100%", transform: "translateY(-18px)" }}
-        ></img>
-        <div
-          className="centered-image-text"
-          style={{
-            color: "white",
-            fontSize: 20,
-            textAlign: "center",
-            flexDirection: "column",
-          }}
-        >
-          <div
-            style={{
-              paddingLeft: "25%",
-              paddingRight: "25%",
-              fontSize: "2.78vw",
-              fontWeight: 600,
-            }}
-          >
-            Empower Farmers, Agricultural Communities And Environmental<br />
-            <span style={{ fontWeight: "normal", fontSize: "1.39vw" }}>
-              Empower Farmers, Agricultural Communities And Environmental
-            </span>
-          </div>
-          <br />
-          <button className="green-button-style">DISCOVER MORE 🠊</button>
-        </div>
-      </div>
+  className="section-1"
+>
+  <img
+    src={image1}
+    alt=""
+    className="section-1-img"
+  />
+  <div
+    className="centered-image-text"
+  >
+    <div className="centered-text">
+      Empower Farmers, Agricultural Communities And Environmental<br />
+      <span className="centered-subtext">
+        Empower Farmers, Agricultural Communities And Environmental
+      </span>
+    </div>
+    <br />
+    <button className="green-button-style">DISCOVER MORE 🠊</button>
+  </div>
+</div>
+
       <div className="section-2">
         <div>
           <img src={image2} alt="" className="section-2-left-image"></img>
